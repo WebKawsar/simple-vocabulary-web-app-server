21c5d3a1b1aa973c0a6b87588b46e64451e9449c
Kawsar Ahmed <web.kawsarahmed@gmail.com>
2020-12-15 22:29:47 -0800

    Done

--- index.js
+++ index.js
@@ -3,14 +3,20 @@ const MongoClient = require('mongodb').MongoClient;
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const ObjectId = require("mongodb").ObjectId;
+require("dotenv").config();
+
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+const port = 8080;
 
-const uri = "mongodb+srv://Kawsar:123456ka@cluster0.u2izr.mongodb.net/vocabularyWebApp?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.u2izr.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 
 //Root url
@@ -21,7 +27,7 @@ app.get("/", (req, res) => {
 
 //Mongodb connect
 client.connect(err => {
-    const wordsCollection = client.db("vocabularyWebApp").collection("vocabularyWords");
+    const wordsCollection = client.db(`${process.env.DB_NAME}`).collection("vocabularyWords");
 
     //Get all words
     app.get("/allWords", (req, res) => {
@@ -69,6 +75,7 @@ client.connect(err => {
 
 
 //port listening
-app.listen(8080, () => {
-    console.log("8080 is port listening the server");
-})
+app.listen(process.env.PORT || port, () => {
+    console.log(`Listening at http://localhost:${port}`);
+});
+  
